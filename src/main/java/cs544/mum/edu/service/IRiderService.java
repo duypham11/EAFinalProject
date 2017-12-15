a08ce63a60e51df5fd185683acd56c8d85651991
duypham11 <duypham11@yahoo.com>
2017-12-15 15:18:51 -0600

    updated

---
+++ src/main/java/cs544/mum/edu/service/IRiderService.java
@@ -0,0 +1,14 @@
+package cs544.mum.edu.service;
+
+import cs544.mum.edu.domain.Rider;
+
+public interface IRiderService {
+	void createRider(Rider rider);
+	Rider getRider(long id);
+	Iterable<Rider> getAllRider();
+	void updateRider(Rider rider);
+	void deleteRider(long id);
+	void deleteRider(Rider rider);
+	Rider findRiderByPhone(String phoneNumber);
+	Iterable<Rider> findRidersByZipCode(String zipcode);
+}
