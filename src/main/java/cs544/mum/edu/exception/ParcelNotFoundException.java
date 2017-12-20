4a90276ddde6bebc3f780b68d881b62d3c175495
duypham11 <duypham11@yahoo.com>
2017-12-20 06:13:30 -0600

    update AOP for sending notifyemail

--- src/main/java/cs544/mum/edu/exception/ParcelNotFoundException.java
+++ src/main/java/cs544/mum/edu/exception/ParcelNotFoundException.java
@@ -3,7 +3,7 @@ package cs544.mum.edu.exception;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(value = HttpStatus.NOT_FOUND, reason = "No products found under thiscategory.")
+@ResponseStatus(value = HttpStatus.NOT_FOUND, reason = "No Parcel found.")
 public class ParcelNotFoundException extends RuntimeException {
 	
 	private String message;
